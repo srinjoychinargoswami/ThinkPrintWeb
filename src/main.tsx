4b6d5fe3f527ff111df848cba22e5ec0a7b528b6
srinjoychinargoswami <srinjoygoswami@gmail.com>
2026-06-23 10:00:38 -0400

    adjusted novus integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,20 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Generate a stable anonymous ID per browser
+function getVisitorId() {
+  let id = localStorage.getItem('tp_visitor_id');
+  if (!id) {
+    id = 'anon-' + crypto.randomUUID();
+    localStorage.setItem('tp_visitor_id', id);
+  }
+  return id;
+}
+
+
 pendo.initialize({
   visitor: {
-    id: ''
+    id: getVisitorId()
   }
 });
 
